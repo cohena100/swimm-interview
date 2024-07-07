24899e2b5d14efb7896e63a37d8c60e4e9629920
Avi Cohen <cohena100@gmail.com>
2024-07-07 11:44:38 +0300

    Tanstack router with React Vite app and React Query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,11 @@
+import { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRouteWithContext, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
   component: () => (
     <>
       <div className="p-2 flex gap-2">
